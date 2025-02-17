bba11b509c075ebff9013ac9b75babb1c083a8ce
Alihan Gedik <71148065+alihangedik@users.noreply.github.com>
2025-02-17 17:37:25 +0300

    reformat

--- lib/yanlis_cevaplar.dart
+++ lib/yanlis_cevaplar.dart
@@ -26,38 +26,40 @@ class YanlisSorularProvider extends ChangeNotifier {
     yanlisSorular.add(soru);
     notifyListeners(); // Önce UI güncelle
     await saveYanlisSorularToPrefs(); // Sonra kaydet
-
   }
 
   // SharedPreferences'tan soruları yükleme
   Future<void> loadYanlisSorularFromPrefs() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<String>? jsonList = prefs.getStringList('yanlisSorular'); // Liste olarak al
+    List<String>? jsonList =
+        prefs.getStringList('yanlisSorular'); // Liste olarak al
 
     if (jsonList != null) {
       try {
         _yanlisSorular = jsonList
-            .map((item) => json.decode(item) as Map<String, dynamic>) // JSON parse et
+            .map((item) =>
+                json.decode(item) as Map<String, dynamic>) // JSON parse et
             .toList();
         notifyListeners();
-
       } catch (e) {
         log("Hata: $e"); // Hata loglaması
       }
     }
   }
+
   // SharedPreferences'a soruları kaydetme
 
   Future<void> saveYanlisSorularToPrefs() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
 
     // Her öğeyi JSON stringe çevir ve liste olarak kaydet
-    List<String> jsonList = _yanlisSorular.map((item) => json.encode(item)).toList();
+    List<String> jsonList =
+        _yanlisSorular.map((item) => json.encode(item)).toList();
 
-    await prefs.setStringList('yanlisSorular', jsonList); // Listeyi SharedPreferences'a kaydet
+    await prefs.setStringList(
+        'yanlisSorular', jsonList); // Listeyi SharedPreferences'a kaydet
 
     notifyListeners();
-
   }
 
   // İşlem türüne göre filtreleme fonksiyonu
@@ -73,7 +75,8 @@ class YanlisSorularTabView extends StatefulWidget {
   State<YanlisSorularTabView> createState() => _YanlisSorularTabViewState();
 }
 
-class _YanlisSorularTabViewState extends State<YanlisSorularTabView> with SingleTickerProviderStateMixin {
+class _YanlisSorularTabViewState extends State<YanlisSorularTabView>
+    with SingleTickerProviderStateMixin {
   late TabController _tabController;
 
   @override
@@ -82,105 +85,153 @@ class _YanlisSorularTabViewState extends State<YanlisSorularTabView> with Single
 
     _tabController = TabController(length: 4, vsync: this);
 
-      setState(() {
-        Provider.of<YanlisSorularProvider>(context, listen: false).loadYanlisSorularFromPrefs();
-        Provider.of<YanlisSorularProvider>(context, listen: false).saveYanlisSorularToPrefs();
-      });
-
+    setState(() {
+      Provider.of<YanlisSorularProvider>(context, listen: false)
+          .loadYanlisSorularFromPrefs();
+      Provider.of<YanlisSorularProvider>(context, listen: false)
+          .saveYanlisSorularToPrefs();
+    });
   }
 
   // Tek bir fonksiyonla Dismissible widget'ı tekrar kullanabilmek
   Widget _buildSoruListesi(String islemTuru) {
     bool isLoading = false;
     return RefreshIndicator(
-      onRefresh: () async{
+      onRefresh: () async {
         setState(() {
-          Provider.of<YanlisSorularProvider>(context, listen: false).loadYanlisSorularFromPrefs();
-
+          Provider.of<YanlisSorularProvider>(context, listen: false)
+              .loadYanlisSorularFromPrefs();
         });
       },
       child: Consumer<YanlisSorularProvider>(
         builder: (context, provider, child) {
           var sorular = provider.filtreleByIslemTuru(islemTuru);
-          return isLoading == true ? CircularProgressIndicator() : sorular.length == 0 ? Center(child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("🎉", style: TextStyle(fontSize: 100)),
-              Text("Bu kategoride \nyanlış soru yapmadın" , style: TextStyle(color: Colors.white , fontSize: 20 , fontWeight: FontWeight.bold ) , textAlign: TextAlign.center),
-            ],
-          ),): Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.info_outline, color: Colors.white54 , size: 18) ,
-                    SizedBox(width: 5),
-                    Text("Silmek için sola kaydır" , style: TextStyle(color: Colors.white54 , fontSize: 12 , fontWeight: FontWeight.bold)),
-                  ],
-                ),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height - 200,
-                child: ListView.builder(
-                  itemCount: sorular.length,
-                  itemBuilder: (context, index) {
-                    var soru = sorular[index];
-                    return Dismissible(
-                      key: Key(soru['soru']),
-                      direction: DismissDirection.endToStart,
-                      onDismissed: (direction) {
-                        provider._yanlisSorular.removeAt(index);
-                        provider.saveYanlisSorularToPrefs();
-                        setState(() {});
-                      },
-                      background: Container(
-                        color: Colors.green,
-                        child: Align(
-                          alignment: Alignment.centerRight,
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 20.0),
-                            child: Text("Bu soruyu anladım", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+          return isLoading == true
+              ? CircularProgressIndicator()
+              : sorular.length == 0
+                  ? Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("🎉", style: TextStyle(fontSize: 100)),
+                          Text("Bu kategoride \nyanlış soru yapmadın",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.center),
+                        ],
+                      ),
+                    )
+                  : Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.info_outline,
+                                  color: Colors.white54, size: 18),
+                              SizedBox(width: 5),
+                              Text("Silmek için sola kaydır",
+                                  style: TextStyle(
+                                      color: Colors.white54,
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold)),
+                            ],
                           ),
                         ),
-                      ),
-                      child: Container(
-
-                        height: 140,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Card(
-
-                            color: Colors.white,
-                            child: Stack(
-                              children: [
-                                Positioned( right: 0, bottom: 0, child: Opacity(opacity: 0.4, child: Image.asset('assets/card_background.png', height: 100, fit: BoxFit.cover))),
-                                ListTile(
-                                  title: Padding(
-                                    padding: const EdgeInsets.symmetric(vertical:  5.0),
-                                    child: Text(soru['soru'] ?? '', style: TextStyle(color: Color(0xff2d2e83), fontSize: 25 , fontWeight: FontWeight.bold)),
-                                  ),
-                                  subtitle: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                          'Senin Cevabın: ${soru['yanlisCevap']}' , style: TextStyle(color: Color(0xff2d2e83) , fontSize: 16 , fontWeight: FontWeight.bold)),
-                                      Text('Doğru Cevap: ${soru['dogruCevap']}' , style: TextStyle(color: Color(0xff2d2e83) , fontSize: 16 , fontWeight: FontWeight.bold)),
-                                    ],
+                        Container(
+                          height: MediaQuery.of(context).size.height - 200,
+                          child: ListView.builder(
+                            itemCount: sorular.length,
+                            itemBuilder: (context, index) {
+                              var soru = sorular[index];
+                              return Dismissible(
+                                key: Key(soru['soru']),
+                                direction: DismissDirection.endToStart,
+                                onDismissed: (direction) {
+                                  provider._yanlisSorular.removeAt(index);
+                                  provider.saveYanlisSorularToPrefs();
+                                  setState(() {});
+                                },
+                                background: Container(
+                                  color: Colors.green,
+                                  child: Align(
+                                    alignment: Alignment.centerRight,
+                                    child: Padding(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 20.0),
+                                      child: Text("Bu soruyu anladım",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold)),
+                                    ),
                                   ),
                                 ),
-                              ],
-                            ),
+                                child: Container(
+                                  height: 140,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Card(
+                                      color: Colors.white,
+                                      child: Stack(
+                                        children: [
+                                          Positioned(
+                                              right: 0,
+                                              bottom: 0,
+                                              child: Opacity(
+                                                  opacity: 0.4,
+                                                  child: Image.asset(
+                                                      'assets/card_background.png',
+                                                      height: 100,
+                                                      fit: BoxFit.cover))),
+                                          ListTile(
+                                            title: Padding(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      vertical: 5.0),
+                                              child: Text(soru['soru'] ?? '',
+                                                  style: TextStyle(
+                                                      color: Color(0xff2d2e83),
+                                                      fontSize: 25,
+                                                      fontWeight:
+                                                          FontWeight.bold)),
+                                            ),
+                                            subtitle: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                    'Senin Cevabın: ${soru['yanlisCevap']}',
+                                                    style: TextStyle(
+                                                        color:
+                                                            Color(0xff2d2e83),
+                                                        fontSize: 16,
+                                                        fontWeight:
+                                                            FontWeight.bold)),
+                                                Text(
+                                                    'Doğru Cevap: ${soru['dogruCevap']}',
+                                                    style: TextStyle(
+                                                        color:
+                                                            Color(0xff2d2e83),
+                                                        fontSize: 16,
+                                                        fontWeight:
+                                                            FontWeight.bold)),
+                                              ],
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
                           ),
                         ),
-                      ),
+                      ],
                     );
-                  },
-                ),
-              ),
-            ],
-          );
         },
       ),
     );
@@ -195,13 +246,22 @@ class _YanlisSorularTabViewState extends State<YanlisSorularTabView> with Single
         leading: IconButton(
           icon: Icon(Icons.arrow_back_rounded, color: Colors.white),
           onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => Mainmenu(yanlisSorular:Provider.of<YanlisSorularProvider>(context, listen: false)._yanlisSorular ,)));
+            Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => Mainmenu(
+                          yanlisSorular: Provider.of<YanlisSorularProvider>(
+                                  context,
+                                  listen: false)
+                              ._yanlisSorular,
+                        )),
+                (route) => false);
           },
         ),
         backgroundColor: Color(0xff2d2e83),
         title: Text("Yanlış Sorular", style: TextStyle(color: Colors.white)),
         bottom: TabBar(
-dividerColor:Color(0xff2d2e83),
+          dividerColor: Color(0xff2d2e83),
           indicatorColor: Colors.white,
           unselectedLabelColor: Colors.white,
           labelColor: Colors.white,
@@ -215,7 +275,6 @@ dividerColor:Color(0xff2d2e83),
         ),
       ),
       body: TabBarView(
-
         controller: _tabController,
         children: [
           // Toplama Soruları Sekmesi
